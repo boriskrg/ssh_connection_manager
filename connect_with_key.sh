5ba97b956e2d04f8345a2f72bfeee2af01b7955a
b <kb2k8@bk.ru>
2020-03-15 08:43:15 +0700

    unset temp vars after session end

--- connect_with_key.sh
+++ connect_with_key.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/bash
+
 echo -ne "\033]30;SSH: $___USER___@$___ALIAS___\007"
-ssh -i $___KEY___ $___USER___@$___HOST___;
-$SHELL;
+
+ssh -i $___KEY___ $___USER___@$___HOST___
+
+unset ___KEY___
+unset ___USER___
+unset ___HOST___
+unset ___ALIAS___
+
+$SHELL
+
--- connect_with_pass.sh
+++ connect_with_pass.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/bash
+
 echo -ne "\033]30;SSH: $___USER___@$___ALIAS___\007"
-sshpass -p $___PASS___ ssh $___USER___@$___HOST___;
-$SHELL;
+
+sshpass -p $___PASS___ ssh $___USER___@$___HOST___
+
+unset ___PASS___
+unset ___USER___
+unset ___HOST___
+unset ___ALIAS___
+
+$SHELL
+
